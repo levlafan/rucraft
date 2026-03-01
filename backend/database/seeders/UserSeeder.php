a617781ef8561574e454d2dd91d8659971359251
Anastasiya <nastya.davydova.2006@mail.ru>
2026-03-02 02:01:26 +0300

    Обновление сидеров

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -51,7 +52,7 @@ class UserSeeder extends Seeder
                 'role' => 'admin',
                 'is_banned' => false,
             ],
-        ]
+        ];
 
         foreach ($users as $userData) {
             User::updateOrCreate(
